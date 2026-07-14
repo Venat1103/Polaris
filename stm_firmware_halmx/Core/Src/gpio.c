7c3267b3529b0d31cc844426e545c6e313e1bddc
Soham <soham.borate1@gmail.com>
2026-07-14 20:18:49 +0530

    Update EXTI Triggers

--- stm_firmware_halmx/Core/Src/gpio.c
+++ stm_firmware_halmx/Core/Src/gpio.c
@@ -79,13 +79,13 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : IMU0_ICM_INT_Pin BARO1_LPS_INT_Pin */
   GPIO_InitStruct.Pin = IMU0_ICM_INT_Pin|BARO1_LPS_INT_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : IMU1_LSM_INT_Pin */
   GPIO_InitStruct.Pin = IMU1_LSM_INT_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(IMU1_LSM_INT_GPIO_Port, &GPIO_InitStruct);
 
